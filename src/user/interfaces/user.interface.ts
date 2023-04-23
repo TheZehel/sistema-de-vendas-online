3709f608445eafd889861859179e79dfa17255c3
TheZehel <alls.yuri@gmail.com>
2023-04-23 14:23:07 -0300

    criptografia de senha adicionado

--- src/user/interfaces/user.interface.ts
+++ src/user/interfaces/user.interface.ts
@@ -1,4 +1,5 @@
-export interface createUserDto {
+export interface User {
+  id: number;
   name: string;
   email: string;
   phone: string;
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dtos/createUser.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { createUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return createUser;
+  async createUser(@Body() createUser: createUserDto): Promise<createUserDto> {
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUser(): Promise<User[]> {
+    return this.userService.getAllUser();
   }
 }
